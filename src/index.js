bd600776c63672b2f1e15ef7f7f1c599756ae802
dankeone <sg10110000@gmail.com>
2020-03-20 20:35:28 +0300

    fix: add new index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+ 	let mn = Infinity;
+ 	if (arguments.length == 0) return(0);
+	for(let i = 0;i < array.length; i++) 
+	if(mn > array[i]) mn = array[i]; 	
+	if (mn == Infinity) return(0); else return(mn);
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+	if (arguments.length == 0) return(0);
+   	let mx = -Infinity;
+	for(let i = 0;i < array.length; i++) 
+	if(mx < array[i]) mx = array[i]; 	
+	if (mx == -Infinity) return(0); else return(mx);
+} 
 
 exports.avg = function avg (array) {
-  return 0;
+	if (arguments.length == 0) return(0);
+  	let res = 0;
+	for(let i = 0;i < array.length; i++) res += array[i];	
+	if (res == 0) return(0); else return(res / array.length);
 }
+
